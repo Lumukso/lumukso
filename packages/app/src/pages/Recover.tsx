47089061e98c01264aaa0e6467071a04e74f8ead
Peter <touhonoob@gmail.com>
2022-08-28 05:46:24 +0800

    add isThisYou page

--- packages/app/src/pages/Recover.tsx
+++ packages/app/src/pages/Recover.tsx
@@ -3,20 +3,20 @@ import {useParams} from "react-router-dom";
 import {useEffect, useRef, useState} from "react";
 import {ethers} from "ethers";
 import {toast, ToastContainer} from "react-toastify";
+import {useLumuksoFactory} from "../hooks/lumukso";
+import {LumuksoSocialRecovery__factory} from "@lumukso/contracts/types/ethers-contracts/factories/LumuksoSocialRecovery__factory";
 import {useUp} from "../hooks/up";
 
 export function Recover() {
+    const lumuksoFactory = useLumuksoFactory();
     const {
-        connect,
-        signer: upSigner,
-        isConnected,
-        isConnecting,
-        address,
-        universalProfileOwner
+        signer
     } = useUp();
-
     let {universalProfileAddress} = useParams();
     const [inputUniversalProfileAddress, setInputUniversalProfileAddress] = useState("");
+    const [universalProfileAddressIsValid, setUniversalProfileAddressIsValid] = useState(false);
+    const [lumuksoSocialRecovery, setLumuksoSocialRecovery] = useState(null);
+    const [nonRecoverableUniversalProfileAddress, setNonRecoverableUniversalProfileAddress] = useState(false);
     const addressInputRef = useRef(null);
     const notifyInvalidUniversalProfileAddress = () => toast("Please input a valid universal profile address", {
         toastId: "notifyInvalidUniversalProfileAddress",
@@ -35,14 +35,43 @@ export function Recover() {
     }, [universalProfileAddress]);
 
     useEffect(() => {
-        if (inputUniversalProfileAddress && !ethers.utils.isAddress(inputUniversalProfileAddress)) {
-            addressInputRef.current.reportValidity();
+        if (inputUniversalProfileAddress) {
+            if (!ethers.utils.isAddress(inputUniversalProfileAddress)) {
+                addressInputRef.current.reportValidity();
+                setUniversalProfileAddressIsValid(false);
+            } else {
+                setUniversalProfileAddressIsValid(true);
+            }
         }
     }, [inputUniversalProfileAddress])
+
+    useEffect(() => {
+        if (lumuksoFactory && universalProfileAddressIsValid && signer) {
+            lumuksoFactory.instances(universalProfileAddress)
+                .then((lumuksoAddress : string | ethers.ContractTransaction) => {
+                    if (lumuksoAddress !== ethers.constants.AddressZero) {
+                        setLumuksoSocialRecovery(LumuksoSocialRecovery__factory.connect(lumuksoAddress.toString(), signer));
+                    } else {
+                        setNonRecoverableUniversalProfileAddress(true);
+                        notifyInvalidUniversalProfileAddress();
+                    }
+                })
+        }
+    }, [lumuksoFactory, universalProfileAddressIsValid, signer])
 
     return (
         <>
             <Layout>
+                {
+                    lumuksoSocialRecovery ? <>
+                        <div className="avatar">
+                            <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                <img src="https://placeimg.com/192/192/people"/>
+                            </div>
+                        </div>
+                    </> : null
+                }
+
                 <input type="text"
                        placeholder="Input your Lukso Universal Profile address to recover"
                        className="input input-bordered input-info w-full"
@@ -53,9 +82,6 @@ export function Recover() {
                        onInvalid={(e) => e.currentTarget.setCustomValidity("Please input a valid contract address")}
                        value={inputUniversalProfileAddress}
                        ref={addressInputRef} />
-                {
-                    // TODO: add "Is this you?"
-                }
             </Layout>
             <ToastContainer position="top-center" autoClose={1000} />
         </>
